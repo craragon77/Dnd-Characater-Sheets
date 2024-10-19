a519b84aa1f0fd1df835df88b78328183e421819
craragon77 <aragonchristopherray@gmail.com>
2024-10-19 12:58:28 -0400

    bones of the stat block are in place

--- src/app/stat_block.tsx
+++ src/app/stat_block.tsx
@@ -1,6 +1,70 @@
-
+'use client'
+import { useState, useEffect } from "react";
 
 export default function statBlock(){
+    const [statBlock, setStatBlock] = useState({
+        'strength': 0,
+        'dexterity': 0,
+        'constitution': 0,
+        'intelligence': 0,
+        'wisdom': 0,
+        'charisma': 0
+    });
+    const [strengthSkills, setStrengthSkills] = useState([
+        { name: 'Saving Throw', proficient: false}, 
+        {name: 'Athletics', proficient: false}
+    ]);
+    const [dexteritySkills, setDexteritySkills] = useState([
+        { name: 'Saving Throw', proficient: false},
+        { name: 'Acrobatics', proficient: false },
+        { name: 'Sleight of Hand', proficient: false },
+        { name: 'Stealth', proficient: false },
+    ]);
+    const [constitutionSkills, setConstitutionSkills] = useState([
+        { name: 'Saving Throw', proficient: false},
+        // { name: 'Resist Poison', proficient: false },
+        // { name: 'Resist Disease', proficient: false },
+        // { name: 'Concentration Checks', proficient: false },
+    ]);
+    const [intelligenceSkills, setIntelligenceSkills] = useState([
+        { name: 'Saving Throw', proficient: false},
+        { name: 'Arcana', proficient: false },
+        { name: 'History', proficient: false },
+        { name: 'Investigation', proficient: false },
+        { name: 'Nature', proficient: false },
+        { name: 'Religion', proficient: false },
+    ]);
+    const [wisdomSkills, setWisdomSkills] = useState([
+        { name: 'Saving Throw', proficient: false},
+        { name: 'Animal Handling', proficient: false },
+        { name: 'Insight', proficient: false },
+        { name: 'Medicine', proficient: false },
+        { name: 'Perception', proficient: false },
+        { name: 'Survival', proficient: false },
+    ]);
+    const [charismaSkills, setCharismaSkills] = useState([
+        { name: 'Saving Throw', proficient: false},
+        { name: 'Deception', proficient: false },
+        { name: 'Intimidation', proficient: false },
+        { name: 'Performance', proficient: false },
+        { name: 'Persuasion', proficient: false },
+    ]);
+    const [profBonus, setProfBonus] = useState(0);
+    const [inpsiration, setInspiration] = useState(null);
+    const [passiveWisdom, setPassiveWisdom] = useState(0);
+
+    useEffect(() => {
+        setStatBlock({
+            'strength': 10,
+            'dexterity': 18,
+            'constitution': 14,
+            'intelligence': 9,
+            'wisdom': 14,
+            'charisma': 14
+        });
+        setProfBonus(2);
+        
+    },[]);
 
     //TO DO : ADD TABLE OF SKILLS; USE CHATGPT TO MAKE THE TABLE
 
@@ -8,73 +72,91 @@ export default function statBlock(){
         let newStat:number;
         switch(true){
             case (score >= 30):
-            return +10; // Maximum modifier for scores above 30
+            return '+10'; // Maximum modifier for scores above 30
             case (score >= 28):
-            return +9;
+            return '+9';
             case (score >= 26):
-            return +8;
+            return '+8';
             case (score >= 24):
-            return +7;
+            return '+7';
             case (score >= 22):
-            return +6;
+            return '+6';
             case (score >= 20):
-            return +5;
+            return '+5';
             case (score >= 18):
-            return +4;
+            return '+4';
             case (score >= 16):
-            return +3;
+            return '+3';
             case (score >= 14):
-            return +2;
+            return '+2';
             case (score >= 12):
-            return +1;
+            return '+1';
             case (score >= 10):
-            return 0;
+            return '0';
             case (score >= 8):
-            return -1;
+            return '-1';
             case (score >= 6):
-            return -2;
+            return '-2';
             case (score >= 4):
-            return -3;
+            return '-3';
             case (score >= 2):
-            return -4;
+            return '-4';
             default:
-            return -5; // For scores below 2
+            return '-5'; // For scores below 2
         }
-
     }
+
+    function listSkills(statSkill: {name: string, proficient: boolean}[]){
+        return statSkill.map((skill, idx) => {
+            return<>
+                <li id={`${statSkill}_${idx}`}>
+                    <div>
+                        <input id={`${skill.name}_${idx}`} type="radio" aria-checked={skill.proficient ? true : false} checked={skill.proficient ? true : false}/>
+                        <label htmlFor={`${skill.name}_${idx}`}>{`${skill.name}_${idx}`}</label>
+                    </div>
+                </li>
+            </>
+        })
+    };
 
     return(
         <>
             <div>
-                <p>Proficiency Bonus:</p>
+                <p>Proficiency Bonus: {profBonus}</p>
                 <p>Inspiration:</p>
             </div>
             <div>
-                <p>Strength: 10</p><br/>
-                <p>0</p>
+                <p>Strength: {statBlock.strength}</p><br/>
+                <p>{formatStats(statBlock.strength)}</p>
+                <ul>{listSkills(strengthSkills)}</ul>
             </div>
             <div>
-                <p>Dexterity: 18</p>
-                <p>+4</p>
+                <p>Dexterity: {statBlock.dexterity}</p>
+                <p>{formatStats(statBlock.dexterity)}</p>
+                <ul>{listSkills(dexteritySkills)}</ul>
             </div>
             <div>
-                <p>Constitution: 14</p>
-                <p>+2</p>
+                <p>Constitution: {statBlock.constitution}</p>
+                <p>{formatStats(statBlock.constitution)}</p>
+                <ul>{listSkills(constitutionSkills)}</ul>
             </div>
             <div>
-                <p>Intelligence: 9</p>
-                <p>0</p>
+                <p>Intelligence: {statBlock.intelligence}</p>
+                <p>{formatStats(statBlock.intelligence)}</p>
+                <ul>{listSkills(intelligenceSkills)}</ul>
             </div>
             <div>
-                <p>Charisma: 18</p>
-                <p>0</p>
+                <p>Wisdom: {statBlock.wisdom}</p>
+                <p>{formatStats(statBlock.wisdom)}</p>
+                <ul>{listSkills(wisdomSkills)}</ul>
             </div>
             <div>
-                <p>Charisma: 18</p>
-                <p>0</p>
+                <p>Charisma: {statBlock.charisma}</p>
+                <p>{formatStats(statBlock.charisma)}</p>
+                <ul>{listSkills(charismaSkills)}</ul>
             </div>
             <div>
-                <p>Passive Wisdon: 18</p>
+                <p>Passive Wisdon: (passiveWisdom)</p>
             </div>
         </>
     )
